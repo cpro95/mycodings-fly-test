78c4531ff0fc14bf651ac1ccfc1e31e5b1f51262
cpro95 <cpro95@gmail.com>
2025-02-09 21:17:14 +0900

    test for vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
-import path from "path";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  console.log(process.env);
+  const isProduction = process.env.NODE_ENV === "production";
+  const resolvedCommand = process.env.npm_lifecycle_event || command;
+  console.log(`isProduction : ${isProduction}`);
+
+  console.log(`command : ${command}`);
+  console.log(`mode : ${mode}`);
+  console.log(`isSsrBuild : ${isSsrBuild}`);
+  console.log(`isPreview : ${isPreview}`);
   if (command === "serve") {
     return {
       css: {
@@ -13,10 +21,8 @@ export default defineConfig(({ command }) => {
           plugins: [tailwindcss, autoprefixer],
         },
       },
-      resolve: {
-        alias: {
-          "./mocks": path.resolve(__dirname, "./mocks"),
-        },
+      define: {
+        __COMMAND__: JSON.stringify(resolvedCommand),
       },
       plugins: [
         reactRouter(),
